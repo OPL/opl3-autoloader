a792539dedc9dde600e72b33ba0703fec1d44b2f
zyxist <nightstorm@tlen.pl>
2011-06-30 15:13:36 +0200

    Adding the Exception subnamespace and the first dedicated exception class.

---
+++ tests/TestSuite/Toolset/AllTests.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The test suite file that configures the execution of the test cases.
+ *
+ * @author Tomasz "Zyx" Jędrzejewski
+ * @copyright Copyright (c) 2009 Invenzzia Group
+ * @license http://www.invenzzia.org/license/new-bsd New BSD License
+ */
+namespace TestSuite;
+
+class AllTests extends \PHPUnit_Framework_TestSuite
+{
+	public static function suite()
+	{
+		$suite = new AllTests('TestSuite');
+
+		$suite->addTestSuite('TestSuite\\ClassMapBuilderTest');
+		$suite->addTestSuite('TestSuite\\GenericLoaderTest');
+		$suite->addTestSuite('TestSuite\\ClassMapLoaderTest');
+		$suite->addTestSuite('TestSuite\\PHARLoaderTest');
+		$suite->addTestSuite('TestSuite\\UniversalLoaderTest');
+		$suite->addTestSuite('TestSuite\\CoreTrackerTest');
+
+		return $suite;
+	} // end suite();
+} // end AllTests;
